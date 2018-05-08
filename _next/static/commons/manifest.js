9f5a4c555396096a344244a2101c77a64a491d15
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2018-05-08 20:30:09 +0300

    Automatic deployment commit

--- _next/static/commons/manifest.js
+++ _next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "86c94250460ad2a6aa75"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "ee5df06750f7a6dac6ae"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_app.js","2":"bundles/pages/_document.js","3":"bundles/pages/_error.js","6":"bundles/pages/book-one.js"}[chunkId]||chunkId) + "-" + {"0":"43dd24395c83fd98c322","1":"b67709f37efac56d12f1","2":"9565ef3e681fa46e439e","3":"b274ccaa21d564af38c5","6":"8162f24821c8ab53e625"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_app.js","2":"bundles/pages/_document.js","3":"bundles/pages/_error.js","5":"bundles/pages/copyright.js","7":"bundles/pages/book-two.js"}[chunkId]||chunkId) + "-" + {"0":"43dd24395c83fd98c322","1":"049abac98cd986b97c07","2":"1e02fdcfab4e2e4f34dd","3":"b274ccaa21d564af38c5","5":"8bbcb9d870ac0805df76","7":"dd8cac98ffa9fe81b0f2"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
